4a168a3eed81cbfb2e0bc82eb93165abb02a40dc
IrynaKovalova88 <Avril19882012@gmail.com>
2021-11-12 23:29:01 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import {fetchCountries} from "./services/fetchCountries"
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+    const name = input.value.trim();
+  if (!name) {
+    countryInfo.innerHTML = "";
+    countryList.innerHTML = "";
+  }
+    fetchCountries(name);
+}
+
